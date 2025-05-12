1323ae020660977eac6d36d892d6665d4645b883
Aleks Rutins <keeper@farthergate.com>
2025-05-12 15:59:33 -0400

    Allow access to dispatch() from element definition

--- mod.ts
+++ mod.ts
@@ -10,66 +10,70 @@
  * @param {[string, (name: string, oldValue: string, newValue: string) => Message][]} observedAttributes - An array of tuples where the first element is the attribute name and the second element is a function that returns a message to dispatch when the attribute changes.
  * @returns {typeof HTMLElement} - The custom HTML element class.
  */
+
+interface ElementSelf<Message> extends Element {
+  dispatch(msg: Message): void;
+}
+
 export default <State, Message extends [string, unknown]>(
-    name: string,
-    template: (self: Element, state: State) => string,
-    initialState: (self: Element) => State,
-    update: (self: Element, state: State, message: Message) => State,
-    observedAttributes: [
-        string,
-        (name: string, oldValue: string, newValue: string) => Message
-    ][] = []
+  name: string,
+  template: (self: ElementSelf<Message>, state: State) => string,
+  initialState: (self: ElementSelf<Message>) => State,
+  update: (self: ElementSelf<Message>, state: State, message: Message) => State,
+  observedAttributes: [
+    string,
+    (name: string, oldValue: string, newValue: string) => Message,
+  ][] = [],
 ): typeof HTMLElement => {
-    const elementClass = class extends HTMLElement {
-        static observedAttributes = observedAttributes.map((it) => it[0]);
+  const elementClass = class
+    extends HTMLElement
+    implements ElementSelf<Message>
+  {
+    static observedAttributes = observedAttributes.map((it) => it[0]);
 
-        #state = initialState(this);
+    #state = initialState(this);
 
-        #render() {
-            this.shadowRoot!.innerHTML = template(this, this.#state);
+    #render() {
+      this.shadowRoot!.innerHTML = template(this, this.#state);
 
-            // Attach event listeners
-            this.shadowRoot!.querySelectorAll("[x-on]").forEach((el) => {
-                const events = new Map(
-                    el
-                        .getAttribute("x-on")!
-                        .split(/\s*,\s*/)
-                        .map((str) => str.split(/\s*=>\s*/)) as [
-                        keyof ElementEventMap,
-                        Message[0]
-                    ][]
-                );
+      // Attach event listeners
+      this.shadowRoot!.querySelectorAll("[x-on]").forEach((el) => {
+        const events = new Map(
+          el
+            .getAttribute("x-on")!
+            .split(/\s*,\s*/)
+            .map((str) => str.split(/\s*=>\s*/)) as [
+            keyof ElementEventMap,
+            Message[0],
+          ][],
+        );
 
-                events.forEach((message, event) =>
-                    el.addEventListener(event, (ev) =>
-                        this.dispatch([message, ev] as Message)
-                    )
-                );
-            });
-        }
+        events.forEach((message, event) =>
+          el.addEventListener(event, (ev) =>
+            this.dispatch([message, ev] as Message),
+          ),
+        );
+      });
+    }
 
-        connectedCallback() {
-            this.attachShadow({ mode: "open" });
-            this.#render();
-        }
+    connectedCallback() {
+      this.attachShadow({ mode: "open" });
+      this.#render();
+    }
 
-        attributeChangedCallback(
-            name: string,
-            oldValue: string,
-            newValue: string
-        ) {
-            const handler = observedAttributes.find((it) => it[0] == name);
-            if (handler) {
-                this.dispatch(handler[1](name, oldValue, newValue));
-            }
-        }
+    attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+      const handler = observedAttributes.find((it) => it[0] == name);
+      if (handler) {
+        this.dispatch(handler[1](name, oldValue, newValue));
+      }
+    }
 
-        dispatch(message: Message) {
-            this.#state = update(this, this.#state, message);
-            this.#render();
-        }
-    };
+    dispatch(message: Message) {
+      this.#state = update(this, this.#state, message);
+      this.#render();
+    }
+  };
 
-    customElements.define(name, elementClass);
-    return elementClass;
+  customElements.define(name, elementClass);
+  return elementClass;
 };
